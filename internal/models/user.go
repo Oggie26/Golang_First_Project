f3ece185b9683f8fdc86c3b9919c569eef1b76bc
Shin <phanhoangnam17@gmail.com>
2026-05-17 17:20:30 +0700

    Update Productr and Category models

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,10 +1,27 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type User struct {
-	ID        string    `json:"id"`
-	Username  string    `json:"username"`
-	Password  string    `json:"-"`
-	CreatedAt time.Time `json:"created_at"`
+	ID        uuid.UUID      `gorm:"primaryKey;type:uuid" json:"id" db:"id"`
+	AccountID uuid.UUID      `gorm:"uniqueIndex;not null;type:uuid" json:"account_id" db:"account_id"`
+	FullName  string         `gorm:"type:varchar(100)" json:"full_name" db:"full_name"`
+	Email     string         `gorm:"type:varchar(100);uniqueIndex" json:"email" db:"email"`
+	Avatar    string         `json:"avatar" db:"avatar"`
+	CreatedAt time.Time      `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at" db:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" db:"deleted_at"`
+}
+
+func (User) TableName() string {
+	return "users"
+}
+
+func init() {
+	RegisterModel(&User{})
 }
